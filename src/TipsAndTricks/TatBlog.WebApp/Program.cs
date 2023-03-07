92302b7f8b78fcaef890766250ffbac4cae9d966
DoQuocSang <2014483@dlu.edu.vn>
2023-03-08 01:21:55 +0700

    Tinh gọn mã nguồn trong tập tin Program.cs

--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeders;
 using TatBlog.Services.Blogs;
+using TatBlog.WebApp.Extensions;
 
 namespace TatBlog.WebApp
 {
@@ -11,59 +12,16 @@ namespace TatBlog.WebApp
         {
             var builder = WebApplication.CreateBuilder(args);
             {
-                builder.Services.AddControllersWithViews();
-
-                builder.Services.AddDbContext<BlogDbContext>(options =>
-                    options.UseSqlServer(
-                        builder.Configuration.GetConnectionString("DefaultConnection")));
-                
-                builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-                builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+                builder
+                    .ConfigureMvc()
+                    .ConfigureServices();
             };
 
             var app = builder.Build();
             {
-                if(app.Environment.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
-                }
-                else
-                {
-                    app.UseExceptionHandler("/Blog/Error");
-
-                    app.UseHsts();
-                }
-
-                app.UseHttpsRedirection();
-
-                app.UseStaticFiles();
-
-                app.UseRouting();
-
-                app.MapControllerRoute(
-                    name: "posts-by-category",
-                    pattern: "blog/category/{slug}",
-                    defaults: new { controller = "Blog", action = "Category" });
-
-                app.MapControllerRoute(
-                    name: "posts-by-tag",
-                    pattern: "blog/tag/{slug}",
-                    defaults: new { controller = "Blog", action = "Tag" });
-
-                app.MapControllerRoute(
-                    name: "single-post",
-                    pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
-                    defaults: new { controller = "Blog", action = "Post" });
-
-                app.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Blog}/{action=Index}/{id?}");
-            }
-
-            using (var scope = app.Services.CreateScope())
-            {
-                var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
-                seeder.Initialize();
+                app.UseRequestPipeLine();
+                app.UseBlogRoutes();
+                app.UseDataSeeder();
             }
 
             app.Run();
